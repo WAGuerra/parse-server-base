e72c86767e134949a990c2eb7ac3ea10526950c1
WAGuerra <wladimir.guerra@gmail.com>
2020-06-12 11:45:39 -0300

    upgrade parse-server and parse-dashboard
    Signed-off-by: WAGuerra <wladimir.guerra@gmail.com>

--- src/parseServer/config.ts
+++ src/parseServer/config.ts
@@ -3,6 +3,11 @@ import path from "path";
 export const databaseUri = '' + process.env.DATABASE_URI + process.env.DATABASE_NAME;
 export const appId = process.env.APP_ID;
 export const masterKey = process.env.MASTER_KEY;
+
+/**
+ * Webpack will compile `./cloud/main.ts` to `./deploy/cloud.js`
+ * @type {string}
+ */
 export const cloudCodeMain = path.join(__dirname,'cloud.js');
 export const serverUrl = `${process.env.SERVER_DOMAIN}${process.env.PARSE_MOUNT}`;
 export const parseDashboardRootPass = process.env.PARSE_DASHBOARD_ROOT_USER_PASSWORD;

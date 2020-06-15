3ac193adbdf66638f0e9d985af35e33f930835db
WAGuerra <wladimir.guerra@gmail.com>
2020-06-15 15:21:23 -0300

    added ParseServer.createLiveQueryServer to bootstrap.ts
    Signed-off-by: WAGuerra <wladimir.guerra@gmail.com>

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,14 +1,20 @@
 /**
  * A initialization code to be run on server startup.
  */
+import http from "http";
 import {createSchemas} from "./parseServer/schema";
 
 
-const bootstrap = () => {
-  createSchemas()
+const ParseServer = require("parse-server").ParseServer;
+
+const bootstrap = (httpServer: http.Server) => {
+  createSchemas();
 
   //TODO Register Parse classes here
 
-}
+// This will enable the Live Query real-time server
+  ParseServer.createLiveQueryServer(httpServer);
 
-export default bootstrap
+};
+
+export default bootstrap;
